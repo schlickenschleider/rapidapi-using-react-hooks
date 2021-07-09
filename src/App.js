23939c3a7f4c445189418e6948340e16c957fee1
schlickenschleider <73256352+schlickenschleider@users.noreply.github.com>
2021-07-09 10:48:05 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useCallback } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  let [responseData, setResponseData] = React.useState('')
+
+  const fetchData = useCallback(() => {
+    axios({
+      "method": "GET",
+      "url": "https://quotes15.p.rapidapi.com/quotes/random/",
+      "headers": {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "quotes15.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+        },
+        "params": {
+        "language_code": "en"
+        }
+    })
+    .then((response) => {
+      setResponseData(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  })
+
+  useEffect(() => {
+    fetchData()
+  },[fetchData])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Famous Quotes Generator</h1>
+        <button type="button" onClick={fetchData}>Quote</button>
       </header>
+      <main>
+        {responseData &&
+          <blockquote>
+            "{responseData && responseData.content}"
+            <small>{responseData && responseData.originator && responseData.originator.name}</small>
+          </blockquote>
+        }
+        </main>
     </div>
   );
 }
